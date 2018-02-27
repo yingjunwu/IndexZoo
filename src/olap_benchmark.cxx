77dfd3fa127ae07dc1053d822880f4f4e6842ec2
Yingjun Wu <yingjun.wu@ibm.com>
2018-02-26 19:24:18 -0800

    update olap benchmark

--- src/olap_benchmark.cxx
+++ src/olap_benchmark.cxx
@@ -21,7 +21,7 @@
 
 void usage(FILE *out) {
   fprintf(out,
-          "Command line options : oltp_benchmark <options> \n"
+          "Command line options : olap_benchmark <options> \n"
           "   -h --help              :  print help message \n"
           "   -t --time_duration     :  time duration (default: 10) \n"
           "   -m --key_count         :  key count (default: 1ull<<20) \n"
@@ -42,7 +42,7 @@ static struct option opts[] = {
 struct Config {
   uint64_t time_duration_ = 10;
   double profile_duration_ = 0.5;
-  uint64_t key_count_ = 1ull<<20;
+  uint64_t key_count_ = 1ull << 20;
   // if unique_key_count_ is set to 0, then generate insert key sequentially.
   uint64_t unique_key_count_ = 0;
   uint64_t reader_count_ = 1;
@@ -87,9 +87,7 @@ void parse_args(int argc, char* argv[], Config &config) {
     }
   }
 
-  if (config.unique_key_count_ != 0) {
-    assert(config.unique_key_count_ <= config.key_count_);
-  }
+  assert(config.unique_key_count_ <= config.key_count_);
 
 }
 
@@ -162,8 +160,6 @@ void run_reader_thread(const uint64_t &thread_id, const Config &config) {
 
   BatchKeys batch_keys(thread_id);
 
-  FastRandom rand_gen(thread_id);
-
   uint64_t &operation_count = operation_counts[thread_id];
   operation_count = 0;
   while (true) {
@@ -185,7 +181,7 @@ void run_reader_thread(const uint64_t &thread_id, const Config &config) {
 void run_workload(const Config &config) {
   
   BatchKeys batch_keys(0);
-  for (size_t i = 0; i < config.init_key_count_; ++i) {
+  for (size_t i = 0; i < config.key_count_; ++i) {
 
     KeyT key = batch_keys.get_insert_key();
     ValueT value = 100;
@@ -195,20 +191,19 @@ void run_workload(const Config &config) {
     data_index->insert(key, offset.raw_data());
   }
 
-  // data_index->reorganize();
+  data_index->reorganize();
 
-  operation_counts = new uint64_t[config.thread_count_];
+  operation_counts = new uint64_t[config.reader_count_];
   uint64_t profile_round = (uint64_t)(config.time_duration_ / config.profile_duration_);
 
   uint64_t **operation_counts_profiles = new uint64_t*[profile_round];
   for (uint64_t round_id = 0; round_id < profile_round; ++round_id) {
-    operation_counts_profiles[round_id] = new uint64_t[config.thread_count_];
-    memset(operation_counts_profiles[round_id], 0, config.thread_count_ * sizeof(uint64_t));
+    operation_counts_profiles[round_id] = new uint64_t[config.reader_count_];
+    memset(operation_counts_profiles[round_id], 0, config.reader_count_ * sizeof(uint64_t));
   }
   std::vector<double> act_size_profiles; // actual allocated size. Unit: GB.
   std::vector<size_t> approx_size_profiles; // approximate data size. Unit: #tuples.
 
-  std::vector<uint64_t> insert_counts; // number of insert operations performed.
   std::vector<uint64_t> read_counts; // number of read operations performed.
 
   double init_mem_size = get_memory_gb();
@@ -219,59 +214,42 @@ void run_workload(const Config &config) {
   std::vector<std::thread> worker_threads;
   uint64_t thread_count = 0;
 
-  // inserter threads
-  for (; thread_count < config.inserter_count_; ++thread_count) {
-    worker_threads.push_back(std::move(std::thread(run_inserter_thread, thread_count, config)));
-  }
   // reader threads
-  for (; thread_count < config.inserter_count_ + config.reader_count_; ++thread_count) {
-    std::cout << "run reader thread" << std::endl;
+  for (; thread_count < config.reader_count_; ++thread_count) {
     worker_threads.push_back(std::move(std::thread(run_reader_thread, thread_count, config)));
   }
 
-  std::cout << "        TIME         INSERT      READ       RAM (act.)   RAM (est.)" << std::endl;
+  std::cout << "        TIME         READ       RAM (act.)   RAM (est.)" << std::endl;
 
   for (uint64_t round_id = 0; round_id < profile_round; ++round_id) {
     std::this_thread::sleep_for(std::chrono::milliseconds(int(config.profile_duration_ * 1000)));
     
-    memcpy(operation_counts_profiles[round_id], operation_counts, sizeof(uint64_t) * config.thread_count_);
+    memcpy(operation_counts_profiles[round_id], operation_counts, sizeof(uint64_t) * config.reader_count_);
 
     act_size_profiles.push_back(get_memory_gb());
     approx_size_profiles.push_back(data_table->size_approx());
     if (round_id == 0) {
       // first round
-      uint64_t insert_count = 0;
       uint64_t read_count = 0;
 
       uint64_t thread_count = 0;
-      // count inserts
-      for (; thread_count < config.inserter_count_; ++thread_count) {
-        insert_count += operation_counts_profiles[0][thread_count];
-      }
+
       // count reads
-      for (; thread_count < config.inserter_count_ + config.reader_count_; ++thread_count) {
+      for (; thread_count < config.reader_count_; ++thread_count) {
         read_count += operation_counts_profiles[0][thread_count];
       }
-      // std::cout << "read count = " << read_count << std::endl;
-      insert_counts.push_back(insert_count);
       read_counts.push_back(read_count);
 
     } else {
       // remaining rounds
-      uint64_t insert_count = 0;
       uint64_t read_count = 0;
 
       uint64_t thread_count = 0;
-      // count inserts
-      for (; thread_count < config.inserter_count_; ++thread_count) {
-        insert_count += operation_counts_profiles[round_id][thread_count] - operation_counts_profiles[round_id - 1][thread_count];
-      }
+      
       // count reads
-      for (; thread_count < config.inserter_count_ + config.reader_count_; ++thread_count) {
+      for (; thread_count < config.reader_count_; ++thread_count) {
         read_count += operation_counts_profiles[round_id][thread_count] - operation_counts_profiles[round_id - 1][thread_count];
       }
-      // std::cout << "read count = " << read_count << std::endl;
-      insert_counts.push_back(insert_count);
       read_counts.push_back(read_count);
     }
 
@@ -283,9 +261,6 @@ void run_workload(const Config &config) {
               << std::setw(5)
               << config.profile_duration_ * (round_id + 1) 
               << " s]:  "
-              << std::setw(5)
-              << insert_counts.at(round_id) * 1.0 / 1000 / 1000 
-              << " M  |  " 
               << std::setw(5)
               << read_counts.at(round_id) * 1.0 / 1000 / 1000 
               << " M  |  " 
@@ -301,17 +276,16 @@ void run_workload(const Config &config) {
   // join all the threads
   is_running = false;
 
-  for (uint64_t i = 0; i < config.thread_count_; ++i) {
+  for (uint64_t i = 0; i < config.reader_count_; ++i) {
     worker_threads.at(i).join();
   }
   
   uint64_t total_count = 0;
-  for (uint64_t i = 0; i < config.thread_count_; ++i) {
+  for (uint64_t i = 0; i < config.reader_count_; ++i) {
     total_count += operation_counts[i];
   }
 
-  std::cout << "insert = " << config.inserter_count_ << ", "
-            << "read = " << config. reader_count_ << ", "
+  std::cout << "read = " << config.reader_count_ << ", "
             << "throughput = " << total_count * 1.0 / config.time_duration_ / 1000 / 1000 << " M ops" 
             << std::endl;
 
@@ -333,7 +307,7 @@ int main(int argc, char* argv[]) {
 
   parse_args(argc, argv, config);
 
-  global_max_key = config.max_key_count_;
+  global_max_key = config.unique_key_count_;
   global_curr_key = 0;
 
   data_table.reset(new DataTable<KeyT, ValueT>());
