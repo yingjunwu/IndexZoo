b4dfe2cebb2f2c5781a786f40d8d8143648984ec
Yingjun Wu <yingjun.wu@ibm.com>
2018-03-14 22:27:00 -0700

    modify time measurer

--- src/time_measurer.h
+++ src/time_measurer.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <iostream>
 #include <chrono>
 using std::chrono::high_resolution_clock;
 using std::chrono::milliseconds;
@@ -8,27 +9,39 @@ using std::chrono::nanoseconds;
 
 class TimeMeasurer {
 public:
-  TimeMeasurer(){}
-  ~TimeMeasurer(){}
+  TimeMeasurer() {}
+  ~TimeMeasurer() {}
 
-  void tic(){
+  void tic() {
     start_time_ = high_resolution_clock::now();
   }
 
-  void toc(){
+  void toc() {
     end_time_ = high_resolution_clock::now();
   }
 
-  long long time_ms(){
+  long long time_ms() const {
     return std::chrono::duration_cast<milliseconds>(end_time_ - start_time_).count();
   }
 
-  long long time_us(){
+  long long time_us() const {
     return std::chrono::duration_cast<microseconds>(end_time_ - start_time_).count();
   }
 
-  long long time_ns(){
+  long long time_ns() const {
     return std::chrono::duration_cast<nanoseconds>(end_time_ - start_time_).count();
+  }
+
+  void print_ms() const {
+    std::cout << std::chrono::duration_cast<milliseconds>(end_time_ - start_time_).count() << " ms" << std::endl;
+  }
+
+  void print_us() const {
+    std::cout << std::chrono::duration_cast<microseconds>(end_time_ - start_time_).count() << " us" << std::endl;
+  }
+
+  void print_ns() const {
+    std::cout << std::chrono::duration_cast<nanoseconds>(end_time_ - start_time_).count() << " ns" << std::endl;
   }
 
 
