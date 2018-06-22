d52e239d7e33dacdd52131ad85cf1a22196d0a2e
Yingjun Wu <yingjun.wu@ibm.com>
2018-06-22 16:04:30 -0700

    working on persist btree

--- design_space/persist_btree.cpp
+++ design_space/persist_btree.cpp
@@ -3,25 +3,27 @@
 #include "persist_btree.h"
 
 int main() {
+
   Storage storage;
-  char *block = new char[BLOCK_SIZE];
-  uint64_t i0 = 100;
-  uint64_t i1 = 110;
-  uint64_t offset = 0;
-  memcpy(block, &i0, sizeof(uint64_t));
-  memcpy(block + 8, &i1, sizeof(uint64_t));
+  PersistVector<uint64_t> vec(&storage);
+  for (size_t i = 100; i > 0; --i) {
+    vec.insert(i, i + 1024);
+  }
 
-  size_t block_id = storage.write_block(block);
+  vec.persist();
 
-  uint64_t i2 = 120;
-  memcpy(block, &i2, sizeof(uint64_t));
+  vec.print();
 
-  block_id = storage.write_block(block);
+  vec.cache();
 
-  char *new_block = new char[BLOCK_SIZE];
-  storage.read_data(block_id, new_block);
-  uint64_t ret = 1111;
-  memcpy(&ret, new_block, sizeof(uint64_t));
-  std::cout << "ret = " << ret << std::endl;
+  vec.print();
+
+  std::vector<uint64_t> values;
+
+  vec.find(99, values);
+
+  for (auto entry : values) {
+    std::cout << entry << std::endl;
+  }
 
 }
--- design_space/persist_btree.h
+++ design_space/persist_btree.h
@@ -3,8 +3,10 @@
 #include <cstdlib>
 #include <vector>
 #include <algorithm>
+#include <cassert>
+#include <unistd.h>
 
-const size_t BLOCK_SIZE = 1024;
+const size_t BLOCK_SIZE = 64 * 1024 * 1024;
 
 class Storage {
 public:
@@ -17,41 +19,168 @@ public:
   }
 
   size_t write_block(const char *data) {
-    size_t ret1 = fwrite(data, BLOCK_SIZE, 1, file_);
+    size_t ret = fwrite(data, BLOCK_SIZE, 1, file_);
+    assert(ret == 1);
+
     fflush(file_);
-    size_t ret = block_count_;
+    fsync(fileno(file_));
+    size_t block_id = block_count_;
     block_count_++;
-    return ret;
+    return block_id;
   }
 
-
-  void read_data(const size_t block_id, char *data) const {
+  void read_block(const size_t block_id, char *data) const {
     fseek(file_, block_id * BLOCK_SIZE, SEEK_SET);
     int ret = fread(data, BLOCK_SIZE, 1, file_);
+    assert(ret == 1);
   }
 
+private:
   FILE *file_;
   size_t block_count_;
 };
 
 
 template<typename KeyT>
-class PersistBTree {
+class PersistVector {
+
+typedef std::pair<KeyT, uint64_t> KVPair;
+
+static bool compare_func(KVPair &lhs, KVPair &rhs) {
+  return lhs.first < rhs.first;
+}
 
 public:
-  PersistBTree(Storage *storage) : storage_(storage) {}
-  ~PersistBTree() {}
+  PersistVector(Storage *storage) : 
+    storage_(storage), 
+    block_(new char[BLOCK_SIZE]),
+    is_persisted_(false) {}
 
+  ~PersistVector() {
+    delete[] block_;
+    block_ = nullptr;
+  }
 
   void insert(const KeyT key, const uint64_t value) {
-    container_.push_back(std::pair<KeyT, uint64_t>(key, value));
+    container_.push_back(KVPair(key, value));
   }
 
-  void organize() {
+  // sort in-memory vector, persist to disk, and clean it up.
+  void persist() {
+    std::sort(container_.begin(), container_.end(), compare_func);
 
+    uint64_t curr_pos = sizeof(uint64_t);
+
+
+    for (auto entry : container_) {
+
+      if (curr_pos + sizeof(KeyT) + sizeof(uint64_t) > BLOCK_SIZE) {
+        // if exceeds BLOCK_SIZE
+        memcpy(block_, &curr_pos, sizeof(uint64_t));
+        
+        size_t block_id = storage_->write_block(block_);
+        block_ids_.push_back(block_id);
+
+        curr_pos = sizeof(uint64_t);
+      }
+
+      memcpy(block_ + curr_pos, &(entry.first), sizeof(KeyT));
+      curr_pos += sizeof(KeyT);
+      memcpy(block_ + curr_pos, &(entry.second), sizeof(uint64_t));
+      curr_pos += sizeof(uint64_t);
+    }
+
+    memcpy(block_, &curr_pos, sizeof(uint64_t));
+
+    size_t block_id = storage_->write_block(block_);
+    block_ids_.push_back(block_id);
+    
+    container_.clear();
+    is_persisted_ = true;
   }
 
-  std::vector<std::pair<KeyT, uint64_t>> container_;
+  // fetch data from disk and store it in in-memory vector.
+  void cache() {
+    for (auto block_id : block_ids_) {
+
+      storage_->read_block(block_id, block_);
+
+      // read block
+      uint64_t max_pos = 0;
+      memcpy(&max_pos, block_, sizeof(uint64_t));
+      uint64_t curr_pos = sizeof(uint64_t);
+      
+      while (curr_pos != max_pos) {
+        assert(curr_pos < max_pos);
+
+        KeyT key;
+        uint64_t value;
+
+        memcpy(&key, block_ + curr_pos, sizeof(KeyT));
+        curr_pos += sizeof(KeyT);
+        memcpy(&value, block_ + curr_pos, sizeof(uint64_t));
+        curr_pos += sizeof(uint64_t);
+
+        container_.push_back(KVPair(key, value));
+      }
+
+    }
+    is_persisted_ = false;
+  }
+
+  void find(const KeyT key, std::vector<uint64_t> &values) {
+    for (auto block_id : block_ids_) {
+      
+      storage_->read_block(block_id, block_);
+
+      // read block
+      uint64_t max_pos = 0;
+      memcpy(&max_pos, block_, sizeof(uint64_t));
+      uint64_t curr_pos = sizeof(uint64_t);
+      
+
+      while (curr_pos != max_pos) {
+        assert(curr_pos < max_pos);
+
+        KeyT load_key;
+        uint64_t load_value;
+
+        memcpy(&load_key, block_ + curr_pos, sizeof(KeyT));
+        curr_pos += sizeof(KeyT);
+        memcpy(&load_value, block_ + curr_pos, sizeof(uint64_t));
+        curr_pos += sizeof(uint64_t);
+
+        if (load_key > key) {
+          return;
+        }
+
+        if (load_key == key) {
+          values.push_back(load_value);
+        }
+      }
+    }
+  }
+
+  void print() const {
+    std::cout << "=================" << std::endl;
+    std::cout << "is persisted: " << (is_persisted_ ? "true" : "false") << std::endl;
+    std::cout << "number of elements in cache is: " << container_.size() << std::endl;
+
+    size_t bound = 5;
+    bound = std::min(container_.size(), bound);
+    for (size_t i = 0; i < bound; ++i) {
+      std::cout << container_.at(i).first << " " << container_.at(i).second << std::endl;
+    }
+
+    std::cout << "=================" << std::endl;
+  }
+  
+  
+private:
+  std::vector<KVPair> container_;
   Storage *storage_;
+  char *block_; // buffer for holding to-be-persisted data
+  std::vector<size_t> block_ids_;
+  bool is_persisted_;
 };
 
