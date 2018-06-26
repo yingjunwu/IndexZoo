4d03d0ce4f25d4a55467cd8526a68a6360ebd82c
Yingjun Wu <yingjun.wu@ibm.com>
2018-06-25 19:06:13 -0700

    update driver

--- persist/persist_driver.cpp
+++ persist/persist_driver.cpp
@@ -1,13 +1,15 @@
 #include "hash_run.h"
 #include "synopsis_run.h"
+
 #include "time_measurer.h"
+#include "fast_random.h"
 
-
-int main() {
+template<typename KeyType, typename RunType>
+void do_persist() {
 
   TimeMeasurer timer;
 
-  HashRun<uint64_t> run(0);
+  RunType run(0);
   for (size_t i = 1000; i > 0; --i) {
     run.insert(i, i + 1024);
   }
@@ -33,5 +35,10 @@ int main() {
   for (auto entry : values) {
     std::cout << entry << std::endl;
   }
+}
 
+
+int main() {
+  do_persist<uint64_t, SynopsisRun<uint64_t>>();
+  do_persist<uint64_t, HashRun<uint64_t>>();
 }
